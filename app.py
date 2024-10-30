a39e11cf62730576807d376b45415e4b22f1dfdd
david <kdavidmacharia8485@gmail.com>
2024-10-30 13:09:58 +0300

    added jinja

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ tasks = [
 #routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
